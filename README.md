cd43e138d2f7c1c6d477ddf83e69eb4b39d20872
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2026-05-12 15:30:50 -0300

    feat: login page with google auth

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Marque os itens conforme for avançando nas semanas:
 
 - [x] Fase 1: Setup e Banco de Dados - Banco criado no Neon, schema definido, migrations rodando e dados de seed visíveis no Prisma Studio.
 - [x] Fase 2: API REST - Todos os endpoints (GET, POST, PATCH, DELETE) funcionando e testados via Thunder Client ou Insomnia.
-- [ ] Fase 3: Autenticação - Login com Google funcionando; usuário só acessa as próprias tarefas.
+- [x] Fase 3: Autenticação - Login com Google funcionando; usuário só acessa as próprias tarefas.
 - [ ] Fase 4: Frontend - Dashboard completo, responsivo e funcionando com dados reais do banco.
 - [ ] Fase 5: Qualidade e Deploy - Aplicação publicada na Vercel com URL pública, código formatado, 1 teste de integração e README documentado.
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,45 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-500">
+      <h1 className="text-4xl font-bold mb-8">Gerenciador de Tarefas</h1>
+      {session ? (
+        <div className="text-center">
+          <img
+            src={session.user?.image || ""}
+            alt={session.user?.name || "profile image"}
+            width={112}
+            height={112}
+            className="rounded-full w-28 h-28 justify-center mb-4 mx-auto"
+          />
+          <p className="mb-4 text-lg">
+            Bem vindo,{" "}
+            <span className="text-sky-500">{session.user?.name}</span>!
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+          <button
+            onClick={() => signOut()}
+            className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            Sair
+          </button>
         </div>
-      </main>
-    </div>
+      ) : (
+        <div>
+          <p>Voce não está logado</p>
+          <button
+            onClick={() => signIn()}
+            className="bg-sky-500 text-white py-2 px-4 rounded-md hover:bg-sky-600"
+          >
+            Entrar com Google
+          </button>
+        </div>
+      )}
+    </main>
   );
 }
